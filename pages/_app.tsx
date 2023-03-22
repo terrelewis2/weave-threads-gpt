60f2861fe6047ebaf6b71ddbd0304775b5949686
terrellewis <terrel.lewis@airblack.co>
2023-03-23 00:22:37 +0530

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
